f905e7f8d1a677812797b4bd9d69f247d419c45e
balthazar-bytes <balirial04@gmail.com>
2025-05-15 21:13:45 -0300

    se agregaron los admin

--- MiPrimerapagina+martinezRial/app_coder/cursos/admin.py
+++ MiPrimerapagina+martinezRial/app_coder/cursos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Curso, Estudiante, Profesor
+
+
+
+admin.site.register(Curso)
+admin.site.register(Estudiante)
+admin.site.register(Profesor)
 
 # Register your models here.
